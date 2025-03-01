4a9e3d3140bb4bc210b28c52ef8693ff145ebe22
Nextjs <tech.united.it@gmail.com>
2025-03-01 16:36:33 +0200

    Add the sidebars components

--- src/app/(other pages)/layout.js
+++ src/app/(other pages)/layout.js
@@ -1,8 +1,12 @@
 
-
+// fonts
 import { Geist, Geist_Mono } from "next/font/google";
-//import "./globals.css";
+// styles
 import ".././globals.css"
+// components
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +29,19 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+
+        <div className='flex justify-between max-w-6xl mx-auto'>
+          <div className='hidden sm:inline border-r h-screen sticky top-0'>
+            <LeftSidebar />
+          </div>
+
+          <div className='w-2xl flex-1'>{children}</div>
+          <div className='lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]'>
+            <RightSidebar />
+          </div>
+        </div>
+
+
       </body>
     </html>
   );
