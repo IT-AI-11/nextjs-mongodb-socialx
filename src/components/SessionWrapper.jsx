56b4a7b97c9831e8e5b08b82c1fa70101ee621ce
Nextjs <tech.united.it@gmail.com>
2025-03-02 18:44:56 +0200

    Final commit

--- src/components/SessionWrapper.jsx
+++ src/components/SessionWrapper.jsx
@@ -1,15 +1,15 @@
 
 
-"use client"
+// "use client"
 
-// for Recoil (for atom global state management)
+// // for Recoil (for atom global state management)
 
-import { RecoilRoot } from 'recoil';
+// import { RecoilRoot } from 'recoil';
 
-// for pop up window
-// only for client side
-// but will cover all the application, works as a wrapper for the entire application
-// <RecoilRoot> goes to (other pages > layout.jsx)
-export default function SessionWrapper({ children }) {
-  return <RecoilRoot>{children}</RecoilRoot>;
-}
+// // for pop up window
+// // only for client side
+// // but will cover all the application, works as a wrapper for the entire application
+// // <RecoilRoot> goes to (other pages > layout.jsx)
+// export default function SessionWrapper({ children }) {
+//   return <RecoilRoot>{children}</RecoilRoot>;
+// }
